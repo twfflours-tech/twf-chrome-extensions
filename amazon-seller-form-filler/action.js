be8329e6bb5dac9b6ac22c3483f2e46822828fe1
Anurag <anurag.131092@gmail.com>
2025-03-21 17:05:22 +0530

    Basic form filling works!

--- amazon-seller-form-filler/action.js
+++ amazon-seller-form-filler/action.js
@@ -1,27 +1,28 @@
 let response;
 let responseJson;
-document.getElementById("fillForm").addEventListener("click", async function() {
-  response = await fetch(chrome.extension.getURL("data.json"));
-  if (response.ok) {
-    responseJson = await response.json();
-  }
-  // console.log(responseJson);
-
-  //grab the text filed value from my tool
-  var intValue = document.getElementById("apivalue").value;
-  // console.log(chrome.extension.getURL("data.json"));
-  chrome.tabs.executeScript(
-    {
-      //send the value to be used by our script
-      code: `var l = console.log.bind(window.console); var dimensions = ${JSON.stringify(
-        responseJson
-      )};`
-    },
-    function() {
-      //run the script in the file injector.js
-      chrome.tabs.executeScript({
-        file: "injector.js"
-      });
+document
+  .querySelector(".fillForm")
+  .addEventListener("click", async function () {
+    response = await fetch(chrome.extension.getURL("data.json"));
+    if (response.ok) {
+      responseJson = await response.json();
     }
-  );
-});
+    // console.log(responseJson);
+
+    //grab the text filed value from my tool
+    // var intValue = document.getElementById("apivalue").value;
+    chrome.tabs.executeScript(
+      {
+        //send the value to be used by our script
+        code: `var l = console.log.bind(window.console); var dimensions = ${JSON.stringify(
+          responseJson
+        )};`,
+      },
+      function () {
+        //run the script in the file injector.js
+        chrome.tabs.executeScript({
+          file: "injector.js",
+        });
+      }
+    );
+  });
--- amazon-seller-form-filler/injector.js
+++ amazon-seller-form-filler/injector.js
@@ -1,101 +1,37 @@
-l(dimensions);
+// l(dimensions);
 
-var tableRows = document.querySelectorAll("table>tr table>tr:last-child");
-// l(tableRows);
+var tableRows = document.querySelectorAll("table>tr table>tr.order-row-item");
 
-tableRows.forEach(tableRow => {
-  var textColumn = tableRow.querySelector("td.a-span5");
-  // l(textColumn);
-  var inputColumn = tableRow.querySelector("td.a-span4");
-  // l(inputColumn);
-  var titleText = textColumn.querySelector(".a-col-right > div:first-of-type")
-    .textContent;
-  l(titleText);
-  var skuAndQuantityDiv = textColumn.querySelector(
-    ".a-col-right > div:last-of-type"
+tableRows.forEach((tableRow) => {
+  var weightColumnInput = tableRow.querySelector(
+    "td.a-span2 input[name='weight']"
   );
+  if (weightColumnInput) {
+    var weight = Number(weightColumnInput.value);
 
-  l(skuAndQuantityDiv.childNodes);
+    // Find the matching dimension based on weight range
+    const matchingDimension = dimensions.find(
+      (dim) => weight >= dim.weightRange[0] && weight <= dim.weightRange[1]
+    );
 
-  var quantityDiv = skuAndQuantityDiv.querySelector(".multi-quantity-badge");
-  var text = "";
-  if (quantityDiv) {
-    text = Number(quantityDiv.textContent.replace("Quantity :", ""));
+    if (matchingDimension) {
+      var lengthInput = tableRow.querySelector(
+        "td.a-span4 input[name='length']"
+      );
+      var widthInput = tableRow.querySelector("td.a-span4 input[name='width']");
+      var heightInput = tableRow.querySelector(
+        "td.a-span4 input[name='height']"
+      );
+
+      fillField(lengthInput, matchingDimension.length);
+      fillField(widthInput, matchingDimension.width);
+      fillField(heightInput, matchingDimension.height);
+    }
   }
-  l(text);
 });
-
-// var lengthInputs;
-// lengthInputs = document.querySelectorAll('input[name="length"]');
-// lengthInputs.forEach(input => {
-//   console.log(input.value);
-// });
-
-// fillField(document.querySelectorAll('input[name="length"]')[0], value)
-function fillforms(data) {
-  fillField(document.querySelector('input[name="fullname"]'), data.title);
-  fillField(document.querySelector('input[name="email"]'), data.author);
-  fillField(document.querySelector('input[name="city"]'), data.pages);
-
-  //form2
-  fillField(document.getElementById("field1"), data.title);
-  fillField(document.querySelector('input[aria-label="field2"]'), data.author);
-  fillField(document.querySelector('input[class="field3"]'), data.pages);
-}
-
-function fillField(selector, value) {
-  var field = selector();
-  fillField(field, value);
-}
 
 function fillField(field, value) {
   if (field) {
     field.value = value;
   }
 }
-
-// function fillforms1(data){
-//     //form1
-//     var fullname = document.querySelector('input[name="fullname"]');
-//     fillField(fullname, data[0]);
-
-//     var email = document.querySelector('input[name="email"]');
-//     fillField(email, data[1]);
-
-//     //city
-//     var city = document.querySelector('input[name="city"]');
-//     fillField(city, data[2]);
-
-//     //form2
-//     var field1 = document.getElementById('field1');
-//     fillField(field1, data[0]);
-
-//     var field2 = document.querySelector('input[aria-label="field2"]');
-//     fillField(field2, data[1]);
-
-//     //probably terrible to look for a class like this..
-//     var field3 = document.querySelector('input[class="field3"]');
-//     fillField(field3, data[2]);
-// }
-
-// {
-//     "title":"The Cat in the Hat",
-//     "author":"Dr. Seuss",
-//     "pages":"48"
-// }
-// function fillforms2(data){
-//     //form1
-//     var title = document.querySelector('input[name="fullname"]');
-//     fillField(title, data.title);
-
-//     var author = document.querySelector('input[name="email"]');
-//     fillField(author, data.author);
-
-//     //form2
-//     var field1 = document.getElementById('field1');
-//     fillField(field1, data.title);
-
-//     //probably terrible to look for a class like this..
-//     var field3 = document.querySelector('input[class="field3"]');
-//     fillField(field3, data.pages)
-// }
