81aa3f6b154c9769a2565c9ca6547374ac5285db
Anurag <anurag.131092@gmail.com>
2025-04-04 13:53:22 +0530

    Grouping by sku child rules

--- amazon-seller-helper/js/action.js
+++ amazon-seller-helper/js/action.js
@@ -40,3 +40,25 @@ document
       }
     );
   });
+
+document
+  .querySelector("#fill-weights-button")
+  .addEventListener("click", async function () {
+    response = await fetch(chrome.extension.getURL("data/data.json"));
+    responseJson = await response.json();
+
+    chrome.tabs.executeScript(
+      {
+        // Send the value to be used by our script
+        code: `var l = console.log.bind(window.console); var dimensions = ${JSON.stringify(
+          responseJson
+        )};`,
+      },
+      function () {
+        // Run the script in the file injector.js
+        chrome.tabs.executeScript({
+          file: "js/injector.js",
+        });
+      }
+    );
+  });
--- amazon-seller-helper/js/summary.js
+++ amazon-seller-helper/js/summary.js
@@ -1,7 +1,19 @@
 window.onload = async function () {
   // let response = await fetch(chrome.extension.getURL("data/sku-data.json"));
-  let response = await fetch(`https://twfflours.com/cdn/shop/t/31/assets/sku-data.json?v=${Date.now()}`);
-  let responseJson = await response.json();
+  let response = await fetch(
+    `https://twfflours.com/cdn/shop/t/31/assets/sku-data-new.json?v=${Date.now()}`
+  );
+  let allSKUData = await response.json();
+
+  const generateTable = (data) => {
+    let table =
+      '<table border="1"><tr><th>SKU</th><th>Description</th><th>Quantity</th></tr>';
+    data.forEach((item) => {
+      table += `<tr><td>${item.SKU}</td><td>${item.description}</td><td align=center>${item.quantity}</td></tr>`;
+    });
+    table += "</table>";
+    return table;
+  };
 
   chrome.storage.local.get("skuData", function (data) {
     console.log(data.skuData);
@@ -12,23 +24,56 @@ window.onload = async function () {
       if (existing) {
         existing.quantity += current.quantity;
       } else {
-        current.description = responseJson[current.SKU];
+        current.description = allSKUData[current.SKU].description;
         acc.push({ ...current });
       }
 
       return acc;
     }, []);
 
-    const generateTable = (data) => {
-      let table =
-        '<table border="1"><tr><th>SKU</th><th>Description</th><th>Quantity</th></tr>';
-      data.forEach((item) => {
-        table += `<tr><td>${item.SKU}</td><td>${item.description}</td><td align=center>${item.quantity}</td></tr>`;
-      });
-      table += "</table>";
-      return table;
-    };
+    const eliminated = [];
 
-    document.querySelector("#summary").innerHTML = generateTable(combinedArray);
+    const filteredArray = combinedArray
+      .reduce((acc, current) => {
+        const existing = acc.find((item) => item.SKU === current.SKU);
+        if (existing) {
+          existing.quantity += current.quantity;
+        } else {
+          const { children } = allSKUData[current.SKU];
+          if (children) {
+            eliminated.push(current.SKU);
+
+            for (const child in children) {
+              const existingChild = acc.find((item) => item.SKU === child);
+              if (existingChild) {
+                existingChild.quantity += current.quantity;
+              } else {
+                acc.push({
+                  SKU: child,
+                  quantity: children[child] * current.quantity,
+                  description: allSKUData[child].description,
+                });
+              }
+            }
+          }
+
+          current.description = allSKUData[current.SKU].description;
+          acc.push({ ...current });
+        }
+
+        return acc;
+      }, [])
+      .filter((item) => !eliminated.includes(item.SKU));
+
+    document.querySelector("#summary").innerHTML = `
+      <div>
+        <h2>Original</h2>
+        ${generateTable(combinedArray)}
+      </div>
+      <div>
+        <h2>Grouped</h2>
+        ${generateTable(filteredArray)}
+      </div>
+    `;
   });
 };
